f8e41a4aedc01b87b8cdd5b850611e7052ae38ad
Nasser Alhouti <bavary2@gmial.com>
2017-12-09 16:13:43 -0700

    reveal points with for each function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,28 @@
-var animatePoints = function(){
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var revealPoint = function(point){
 
-  var revealPoint = function(index){
+      point.style.opacity= 1;
+      point.style.transform = "scaleX(1) translateY(0)";
+      point.style.msTransform = "scaleX(1) translateY(0)";
+      point.style.WebKitTransform = "scaleX(1) translateY(0)";
+    }
 
-        points[index].style.opacity= 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebKitTransform = "scaleX(1) translateY(0)";
-      }
-        for (var i = 0; i < points.length; i++) {
-            revealPoint(i);
-  }
+ var animatePoints = function(points){
+forEach(points , revealPoint);
 
+
+
+
+}
+window.onload = function(){
+  var sellingpoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingpoints.getBoundingClientRect().top - window.innerHeight + 200;
+window.addEventListener('scroll',function(event) {
+ if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+       animatePoints(pointsArray);
+ }if(window.innerHeight > 950){
+    animatePoints(pointsArray);
+ }
+  });
 }
