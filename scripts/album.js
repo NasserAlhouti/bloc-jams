06376ee310f2ada915aa613f4d97d797202e419f
Nasser Alhouti <bavary2@gmial.com>
2018-02-11 01:11:51 -0700

    changed variables

--- scripts/album.js
+++ scripts/album.js
@@ -1,88 +1,4 @@
-var albumPicasso = { // the names of the albums
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  label: 'Cubism',
-  year: '1881',
-  albumArtUrl: 'assets/images/album_covers/01.png',
-  songs: [{
-      title: 'Blue',
-      duration: '4:26'
-    },
-    {
-      title: 'Green',
-      duration: '3:14'
-    },
-    {
-      title: 'Red',
-      duration: '5:01'
-    },
-    {
-      title: 'Pink',
-      duration: '3:21'
-    },
-    {
-      title: 'Magenta',
-      duration: '2:15'
-    }
-  ]
-};
-//Another album example
-var albumMarconi = {
-  title: 'The Telephone',
-  artist: 'Guglielmo Marconi',
-  label: 'EM',
-  year: '1909',
-  albumArtUrl: 'assets/images/album_covers/20.png',
-  songs: [{
-      title: 'Hello, Operator?',
-      duration: '1:01'
-    },
-    {
-      title: 'Ring, ring, ring',
-      duration: '5:01'
-    },
-    {
-      title: 'Fits in your pocket',
-      duration: '3:21'
-    },
-    {
-      title: 'Can you hear me now?',
-      duration: '3:14'
-    },
-    {
-      title: 'Wrong phone number',
-      duration: '2:15'
-    }
-  ]
-};
-var albumNasser = {
-  title: "Greatest hits",
-  artist: 'Nasser alhouti',
-  label: 'studio ghibli',
-  year: "2012",
-  albumArtUrl: 'assets/images/album_covers/15.png',
-  songs: [{
-      title: 'Where do you take your bar?',
-      duration: '4:01'
-    },
-    {
-      title: 'I left my wallet at your house',
-      duration: '2:04'
-    },
-    {
-      title: "Finding nirvana",
-      duration: '3:04'
-    },
-    {
-      title: "Train like a ninja",
-      duration: '1:30'
-    },
-    {
-      title: "The day is over?",
-      duration: '6:02'
-    }
-  ]
-};
+
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
     '<tr class="album-view-song-item">' + //output table row class "album-view-song-item "
@@ -94,27 +10,28 @@ var createSongRow = function(songNumber, songName, songLength) {
     var clickHandler = function(){ // function bracket
       var songNumber = $(this).attr('data-song-number');
 
-	if (currentlyPlayingSong !== null) { //condition bracket
+	if (currentlyPlayingSongNumber !== null) { //condition bracket
 		// Revert to song number for currently playing song because user started playing new song.
-		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-		currentlyPlayingCell.html(currentlyPlayingSong);
+		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+		currentlyPlayingCell.html(currentlyPlayingSongNumber);
 	} // end of condition
-	if (currentlyPlayingSong !== songNumber) { // condition function
+	if (currentlyPlayingSongNumber !== songNumber) { // condition function
 		// Switch from Play -> Pause button to indicate new song is playing.
 		$(this).html(pauseButtonTemplate);
-		currentlyPlayingSong = songNumber;
+		currentlyPlayingSongNumber = songNumber;
 	} // condition end
   else if (currentlyPlayingSong === songNumber) { // else condition
 		// Switch from Pause -> Play button to pause currently playing song.
 		$(this).html(playButtonTemplate);
-		currentlyPlayingSong = null;
+		currentlyPlayingSongNumber = null;
+    currentSongFromAlbum = null;
 	}// end of else condition
 }; // function end
     var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
 
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(playButtonTemplate);
         }
     };
@@ -123,7 +40,7 @@ var createSongRow = function(songNumber, songName, songLength) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
 
-        if (songNumber !== currentlyPlayingSong) {
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(songNumber);
         }
     }
@@ -135,6 +52,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 // first function
 var setCurrentAlbum = function(album) {
   // #1 setting up the variables
+  currentAlbum = album;
   var $albumTitle = $('.album-view-title');
   var $albumArtist = $('.album-view-artist');
   var $albumReleaseInfo = $('.album-view-release-info');
@@ -161,6 +79,8 @@ var index = 1;
 var pauseButtonTemplate = '<a class="album-song-button"><span class ="ion-pause"></span></a>';
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var currentlyPlayingSong = null;
+var currentAlbum = null;
+var currentlyPlayingSongNumber =null;
 $(document).ready( function() {
   setCurrentAlbum(albumPicasso); //when the page loads album picasso loads
 
---
+++ scripts/fixtures.js
@@ -0,0 +1,86 @@
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [{
+      title: 'Blue',
+      duration: '4:26'
+    },
+    {
+      title: 'Green',
+      duration: '3:14'
+    },
+    {
+      title: 'Red',
+      duration: '5:01'
+    },
+    {
+      title: 'Pink',
+      duration: '3:21'
+    },
+    {
+      title: 'Magenta',
+      duration: '2:15'
+    }
+  ]
+};
+//Another album example
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [{
+      title: 'Hello, Operator?',
+      duration: '1:01'
+    },
+    {
+      title: 'Ring, ring, ring',
+      duration: '5:01'
+    },
+    {
+      title: 'Fits in your pocket',
+      duration: '3:21'
+    },
+    {
+      title: 'Can you hear me now?',
+      duration: '3:14'
+    },
+    {
+      title: 'Wrong phone number',
+      duration: '2:15'
+    }
+  ]
+};
+var albumNasser = {
+  title: "Greatest hits",
+  artist: 'Nasser alhouti',
+  label: 'studio ghibli',
+  year: "2012",
+  albumArtUrl: 'assets/images/album_covers/15.png',
+  songs: [{
+      title: 'Where do you take your bar?',
+      duration: '4:01'
+    },
+    {
+      title: 'I left my wallet at your house',
+      duration: '2:04'
+    },
+    {
+      title: "Finding nirvana",
+      duration: '3:04'
+    },
+    {
+      title: "Train like a ninja",
+      duration: '1:30'
+    },
+    {
+      title: "The day is over?",
+      duration: '6:02'
+    }
+  ]
+};
+var albums = [albumPicasso, albumMarconi, albumNasser];
